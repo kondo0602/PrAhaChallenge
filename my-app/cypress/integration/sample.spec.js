cb4557d4e2f23297df9c8ee18678ef140d4b2cb8
kondo0602 <kondo8363@gmail.com>
2022-02-17 23:28:07 +0900

    test: 引き分けのテストを追加

--- my-app/cypress/integration/sample.spec.js
+++ my-app/cypress/integration/sample.spec.js
@@ -10,4 +10,20 @@ describe("OXゲームのテスト", () => {
 
 		cy.contains("Winner: X");
 	});
+
+	it("OXいずれも3つ揃わなかった場合、「Draw!」が表示されること", () => {
+		cy.visit("http://localhost:3000/");
+
+		cy.get(".data-e2e-0").click();
+		cy.get(".data-e2e-1").click();
+		cy.get(".data-e2e-2").click();
+		cy.get(".data-e2e-3").click();
+		cy.get(".data-e2e-5").click();
+		cy.get(".data-e2e-4").click();
+		cy.get(".data-e2e-7").click();
+		cy.get(".data-e2e-8").click();
+		cy.get(".data-e2e-6").click();
+
+		cy.contains("Draw!");
+	});
 });
