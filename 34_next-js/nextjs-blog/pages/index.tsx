f2d93cf2787916fca4960c701d082d6bdcc7e9b4
kondo0602 <kondo8363@gmail.com>
2022-05-04 00:11:48 +0900

    feat: フッタを作成

--- 34_next-js/nextjs-blog/pages/index.tsx
+++ 34_next-js/nextjs-blog/pages/index.tsx
@@ -2,6 +2,26 @@ import Header from '../components/templates/header';
 import PageHeader from '../components/organisms/page-header';
 import Card from '../components/organisms/card';
 
+const footerStyle: React.CSSProperties = {
+  position: 'fixed',
+  bottom: '0px',
+  left: '0px',
+  right: '0px',
+  zIndex: '40',
+  paddingLeft: '1rem',
+  paddingRight: '1rem',
+  paddingTop: '0.75rem',
+  paddingBottom: '0.75rem',
+  textAlign: 'center',
+  color: 'rgba(255, 255, 255, 1)',
+  backgroundColor: 'rgba(31, 41, 55, 1)',
+};
+
+const footerTextStyle: React.CSSProperties = {
+  color: 'rgba(229, 231, 235, 1)',
+  textDecoration: 'underline',
+};
+
 const outerStyle: React.CSSProperties = {
   overflowX: 'hidden',
   backgroundColor: 'rgba(243, 244, 246, 1)',
@@ -27,39 +47,45 @@ const innerStyle3: React.CSSProperties = {
 
 export default function Home() {
   return (
-    <div style={outerStyle}>
-      <Header />
-      <div style={innerStyle1}>
-        <div style={innerStyle2}>
-          <div style={innerStyle3}>
-            <PageHeader text="Post" />
-            <Card
-              timestamp="Jun 1, 2020"
-              message="Laravel"
-              titleText="Build Your New Idea with Laravel Freamwork."
-              contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
-              text="Read more"
-              imgPath=""
-              userName="AlexJohn"
-            />
-            <Card
-              timestamp="Jun 1, 2020"
-              message="Laravel"
-              titleText="Build Your New Idea with Laravel Freamwork."
-              contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
-              text="Read more"
-              imgPath=""
-              userName="AlexJohn"
-            />
-            <Card
-              timestamp="Jun 1, 2020"
-              message="Laravel"
-              titleText="Build Your New Idea with Laravel Freamwork."
-              contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
-              text="Read more"
-              imgPath=""
-              userName="AlexJohn"
-            />
+    <div>
+      <div style={footerStyle}>
+        This a Blog Page by Shun Kondo.
+        <a style={footerTextStyle}>Component details</a>
+      </div>
+      <div style={outerStyle}>
+        <Header />
+        <div style={innerStyle1}>
+          <div style={innerStyle2}>
+            <div style={innerStyle3}>
+              <PageHeader text="Post" />
+              <Card
+                timestamp="Jun 1, 2020"
+                message="Laravel"
+                titleText="Build Your New Idea with Laravel Freamwork."
+                contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
+                text="Read more"
+                imgPath=""
+                userName="AlexJohn"
+              />
+              <Card
+                timestamp="Jun 1, 2020"
+                message="Laravel"
+                titleText="Build Your New Idea with Laravel Freamwork."
+                contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
+                text="Read more"
+                imgPath=""
+                userName="AlexJohn"
+              />
+              <Card
+                timestamp="Jun 1, 2020"
+                message="Laravel"
+                titleText="Build Your New Idea with Laravel Freamwork."
+                contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
+                text="Read more"
+                imgPath=""
+                userName="AlexJohn"
+              />
+            </div>
           </div>
         </div>
       </div>
