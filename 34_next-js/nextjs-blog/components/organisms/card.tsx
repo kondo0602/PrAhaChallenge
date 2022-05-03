f968512ad5ffbcf8358f1c1d9937a2124724effa
kondo0602 <kondo8363@gmail.com>
2022-05-03 22:24:28 +0900

    feat: カードを作成

---
+++ 34_next-js/nextjs-blog/components/organisms/card.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react';
+import CardHeader from './card-header';
+import CardBody from './card-body';
+import CardFooter from './card-footer';
+
+const outerStyle: React.CSSProperties = {
+  marginTop: '1.5rem',
+};
+
+const innerStyle: React.CSSProperties = {
+  maxWidth: '56rem',
+  paddingLeft: '2.5rem',
+  paddingRight: '2.5rem',
+  paddingTop: '1.5rem',
+  paddingBottom: '1.5rem',
+  margin: 'auto',
+  borderRadius: '0.5rem',
+  boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+};
+
+const Card: FC<{
+  timestamp: string;
+  message: string;
+  titleText: string;
+  contentText: string;
+  text: string;
+  imgPath: string;
+  userName: string;
+}> = ({
+  timestamp,
+  message,
+  titleText,
+  contentText,
+  text,
+  imgPath,
+  userName,
+}) => {
+  return (
+    <div style={outerStyle}>
+      <div style={innerStyle}>
+        <CardHeader timestamp={timestamp} message={message} />
+        <CardBody titleText={titleText} contentText={contentText} />
+        <CardFooter text={text} imgPath={imgPath} userName={userName} />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- 34_next-js/nextjs-blog/pages/index.tsx
+++ 34_next-js/nextjs-blog/pages/index.tsx
@@ -1,16 +1,17 @@
-import CardHeader from '../components/organisms/card-header';
-import CardBody from '../components/organisms/card-body';
-import CardFooter from '../components/organisms/card-footer';
+import Card from '../components/organisms/card';
 
 export default function Home() {
   return (
     <div className="container">
-      <CardHeader timestamp="Jun 1, 2020" message="Laravel" />
-      <CardBody
+      <Card
+        timestamp="Jun 1, 2020"
+        message="Laravel"
         titleText="Build Your New Idea with Laravel Freamwork."
         contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!"
+        text="Read more"
+        imgPath=""
+        userName="AlexJohn"
       />
-      <CardFooter text="Read more" imgPath="" userName="AlexJohn" />
     </div>
   );
 }
